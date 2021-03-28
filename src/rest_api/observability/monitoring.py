96f14ed6832d9bd75d41a4ef7a0c8797b2b87c54
Fábio Pereira <42782608+fbiopereira@users.noreply.github.com>
2021-03-28 20:06:44 -0300

    Update monitoring.py

--- src/rest_api/observability/monitoring.py
+++ src/rest_api/observability/monitoring.py
@@ -7,49 +7,46 @@ from src.custom_log.log_request import log_request
 from src.app import flask_app
 from src.helpers.datetime_helpers import DateTimeHelpers
 
-@log_request
+
 @ns_observability.route('/metrics')
 @api.response(200, 'Prometheus Metrics')
 class Metrics(Resource):
 
-    def get(self):
-        return Response(generate_latest())
-#
-#
-# @log_request
-# @ns_observability.route('/healthcheck')
-# @api.response(200, 'Liveness status')
-# class Liveness(Resource):
-#
-#     def get(self):
-#         return Response("I'm alive")
-#
-#
-# info_model = ns_observability.model('InfoReturnModel', {
-#                     'ENVIRONMENT': fields.String(),
-#                     'SERVICE_NAME': fields.String(),
-#                     'SERVICE_VERSION': fields.String(),
-#                     'SERVER_DATETIME': fields.String(),
-#                     'ENVIRONMENT VARIABLES': fields.String(),
-#                 })
-#
-#
-# @log_request
-# @ns_observability.route('/info')
-# @api.response(200, 'Environment info')
-# class Info(Resource):
-#
-#     @ns_observability.marshal_with(info_model)
-#     def get(self):
-#         info_return = {
-#             "version": flask_app.config['SERVICE_VERSION'],
-#             "environment": flask_app.config['ENVIRONMENT'],
-#             "server_datetime": DateTimeHelpers.get_server_datetime(),
-#             "environment_variables": [
-#                 {"LOG_PATH": flask_app.config['LOG_PATH']},
-#                 {"SERVICE_NAME": flask_app.config['SERVICE_NAME']}
-#             ]
-#         }
-#
-#         return info_return, 200
+     def get(self):
+         return Response(generate_latest())
 
+
+@log_request
+@ns_observability.route('/healthcheck')
+@api.response(200, 'Liveness status')
+class Liveness(Resource):
+
+    def get(self):
+        return Response("I'm alive"), 200
+
+
+info_model = ns_observability.model('InfoReturnModel', {
+                    'ENVIRONMENT': fields.String(),
+                    'SERVICE_NAME': fields.String(),
+                    'SERVICE_VERSION': fields.String(),
+                    'SERVER_DATETIME': fields.String(),
+                    'ENVIRONMENT VARIABLES': fields.String(),
+                 })
+
+
+@log_request
+@ns_observability.route('/info')
+@api.response(200, 'Environment info')
+class Info(Resource):
+
+    def get(self):
+        info_return = {
+            "version": flask_app.config['SERVICE_VERSION'],
+            "environment": flask_app.config['ENVIRONMENT'],
+            "server_datetime": DateTimeHelpers.get_server_datetime(),
+            "environment_variables": [
+                {"LOG_PATH": flask_app.config['LOG_PATH']},
+                {"SERVICE_NAME": flask_app.config['SERVICE_NAME']}
+             ]}
+
+        return info_return, 200
